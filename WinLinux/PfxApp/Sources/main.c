28a40ccbe1cd8a3fd9849dffb98b7039ef4751ef
ego1155 <ego1155@gmail.com>
2021-07-30 21:04:59 -0700

    update

--- WinLinux/PfxApp/Sources/main.c
+++ WinLinux/PfxApp/Sources/main.c
@@ -2,6 +2,11 @@
 
 int main(int argc, char *argv[])
 {
-  
+	p_libsys_init();
+	printf("test ok...\n");
+	printf("test ok...\n");
+	printf("test ok...\n");
+	p_libsys_shutdown();
+	
 	return 0;
 }
--- WinLinux/PfxApp/Sources/plibsys/plibsys_MacOsX.sh
+++ WinLinux/PfxApp/Sources/plibsys/plibsys_MacOsX.sh
@@ -8,5 +8,18 @@ mkdir ./macosx
 cd ./macosx
 cmake -DCMAKE_BUILD_TYPE=Release ../plibsys
 make
+cd ..
+prefix="./plibsys/src/"
+echo "" > "$prefix"plibsysconfig.h
+for file in "`find "$prefix" -name "p*.h"`"
+do {
+	file=${file//$prefix/}
+	for f in $file
+	do {
+		echo "$f"
+		#find "$prefix" -type f -exec sed -i "s+#include <perror.h>+#include \"perror.h\"+g" {} +
+		find "$prefix" -type f -exec sed -i "s+#include <$f>+#include \"$f\"+g" {} +
+	} done
+} done
 
 cd "$curDir"

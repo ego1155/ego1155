aa5fbcd861d8b5891e2f6e9a61b8b9af488d40dc
ego1155 <ego1155@gmail.com>
2021-07-30 22:48:17 -0700

    update

--- WinLinux/PfxApp/Sources/plibsys/plibsys_MacOsX.sh
+++ WinLinux/PfxApp/Sources/plibsys/plibsys_MacOsX.sh
@@ -11,15 +11,16 @@ make
 cd ..
 prefix="./plibsys/src/"
 echo "" > "$prefix"plibsysconfig.h
-for file in "`find "$prefix" -name "p*.h"`"
-do {
-	file=${file//$prefix/}
-	for f in $file
-	do {
-		echo "$f"
+find "$prefix" -name "p*.h"
+#for file in "`find "$prefix" -name "p*.h"`"
+#do {
+	#file=${file//$prefix/}
+	#for f in $file
+	#do {
+		#echo "$f"
 		#find "$prefix" -type f -exec sed -i "s+#include <perror.h>+#include \"perror.h\"+g" {} +
-		find "$prefix" -type f -exec sed -i "s+#include <$f>+#include \"$f\"+g" {} +
-	} done
-} done
+		#find "$prefix" -type f -exec sed -i "s+#include <$f>+#include \"$f\"+g" {} +
+	#} done
+#} done
 
 cd "$curDir"

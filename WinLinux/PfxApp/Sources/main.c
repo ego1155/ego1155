01f197c0b0aa93e87fafc0c9fd49f97fbc6883dc
ego1155 <ego1155@gmail.com>
2021-07-31 17:35:00 -0700

    update

--- WinLinux/PfxApp/Sources/main.c
+++ WinLinux/PfxApp/Sources/main.c
@@ -31,23 +31,52 @@ int main(int argc, char *argv[])
 	for (int i=0; i < size; i++)
 		printf("%d", (int)*(buffer+i));
 	printf("\n");
-
-	for (int i=0; i < size; i++)
-		printf("%d = %d = %d\n", i, (int)*(buffer+i), log10((int)*(buffer+i)) + 1);
 	
-	int x = 12345;
-    int numOfDigits = log10(x) + 1;
-    char* arr = calloc(numOfDigits, sizeof(char));
-    sprintf(arr, "%d", x);
-	printf("\n");
-	printf("%s\n", (char*)arr);
-	free(arr);
-	//int* arrInt = (int*)malloc(size * sizeof(int));
-	//for(int i=0; i < size; i++)
-		//arrInt[i] = (int)*(buffer+i);
-	//for (int i=0; i < size; i++)
-		//printf("%d", (int)*(arrInt+i));
+	char* arrChar = NULL;
+	int idx = 0;
+	for (int i=0; i < size; i++)
+	{
+		int x = (int)*(buffer+i);
+		printf("%d\n", x);
+		int numOfDigits = log10(x) + 1;
+		if (numOfDigits < 1) numOfDigits=1;
+		int ext = (i==(size-1))?0:1;
+		if (!arrChar)
+		{
+			arrChar = malloc((numOfDigits + ext) * sizeof(char));
+			char* arr = calloc(numOfDigits, sizeof(char));
+			sprintf(arr, "%d", x);
+			for (int j=0; j < numOfDigits; j++)
+				arrChar[idx + j] = arr[j];
+			free(arr);
+			if (ext != 0) arrChar[idx + numOfDigits] = ',';
+		}
+		else
+		{
+			arrChar = realloc(arrChar, (idx + numOfDigits + ext) * sizeof(char));
+			char* arr = calloc(numOfDigits, sizeof(char));
+			sprintf(arr, "%d", x);
+			for (int j=0; j < numOfDigits; j++)
+				arrChar[idx + j] = arr[j];
+			free(arr);
+			if (ext != 0) arrChar[idx + numOfDigits] = ',';
+		}
+		idx += numOfDigits + ext;
+	}
+	arrChar = realloc(arrChar, (idx) * sizeof(char));
+	arrChar[idx] = '\0';
+	printf("idx : %d\n", idx);
+	printf("const unsigned char data[] = { %s };\n", (char*)arrChar);
+	free(arrChar);
+
+	//int x = 12345;
+    //int numOfDigits = log10(x) + 1;
+    //char* arr = calloc(numOfDigits, sizeof(char));
+    //sprintf(arr, "%d", x);
 	//printf("\n");
+	//printf("%s\n", (char*)arr);
+	//free(arr);
+	
 	
 	//char* arrChar = (char*)malloc(size * sizeof(char));
 	//for (int i=0; i < size; i++)

